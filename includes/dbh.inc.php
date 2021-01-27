5029e41aba001ff9529a864899fc494e27c525e2
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-01-27 09:40:12 -0500

    I learn how to connect data on my database

---
+++ includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+$dbServaname = "localhost";
+$dbUsername =  "root";
+$dbPassword = "";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServaname,$dbUsername,$dbPassword,$dbName );
+
--- index.php
+++ index.php
@@ -1,29 +1,14 @@
- <?php 
-session_start();
+<?php
+include_once 'includes/dbh.inc.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>title of the document</title>
-    <link rel = "stylesheet type = "text/css href = "style.css"> 
+    <title>Document</title>
 </head>
 <body>
-<h2> I learnt how to use Session in php</h2>
-<ul>
-<li><a href = "index.php">Home</a></li>
-<li><a href = "contact.php">contact</a></li>
-</ul>
-    <?php
-    $_SESSION['username'] = "13";
-    echo $_SESSION['username'] ;
-    if(!isset($_SESSION["username"])){
-        echo "you are not logged in !";
-    }elseif(!isset($_SESSION["lastname"])) {
-        echo "you are logged out!";
-    }
-    ?>
+    $conn
 </body>
 </html>

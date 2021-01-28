f4ae1ec98df2bf7f84fa1065ffe3894f0f8df102
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-01-28 02:21:10 -0500

    i have learnt how to do o to prepare the prepared statement

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -7,8 +7,20 @@ $uid = mysqli_real_escape_string($conn,$_POST['uid']);
 $pwd = mysqli_real_escape_string($conn,$_POST['pwd']);
  
    $sql = "INSERT INTO users (user_first, user_last,user_email, user_uid,
-   user_pwd) VALUES('$first', '$last','$email', '$uid','$pwd');";
-     mysqli_query($conn, $sql);
+   user_pwd) VALUES(?, ?,?, ?,?);";
+   // create the prepared statement
+     $stmt = mysqli_stmt_init($conn);
+     //prepare the prepared statement
+     if(!mysqli_stmt_prepare($stmt,$sql)) {
+         echo "SQL error";
+     }else {
+          // bind paremeters to the placeholder
+         mysqli_stmt_bind_param($stmt, "sssss", $first, $last,$email,$uid,$pwd);
+        //Run parameters to the placeholder
+         mysqli_stmt_execute($stmt);
+     }
 
    header("Location: ../index.php?signup=success");
    ?>
+
+  
---
+++ index2.php
@@ -0,0 +1,33 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $data = "Admin";
+    //created a template
+    $sql = "SELECT * FROM users WHERE user_uid =?;";
+    // create a prepared statement
+    $stmt = mysqli_stmt_init($conn);
+    //prepare the prepared statement
+    if(!mysqli_stmt_prepare($stmt,$sql)) {
+       echo "SQL statement failed ";
+    }else {
+    // bind paremeters to the placeholder
+    mysqli_stmt_bind_param($stmt,"s", $data);
+    //Run parameters to the placeholder
+    mysqli_stmt_execute($stmt);
+    $result = mysqlil_stmt_get_result($stmt);
+       while($row = mysqli_fetch_assoc($result)){
+             echo $row['user_uid' . "br"];
+         }
+    }
+    
+      
+     
+    ?>
+</body>
+</html>

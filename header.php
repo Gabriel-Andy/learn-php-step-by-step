7d389a10fa5acf69c147bf2d9c6f30fefe7ae066
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-01-29 08:45:43 -0500

    I added few images

--- header.php
+++ header.php
@@ -1,7 +1,3 @@
-<?php
-  session_start();
-  include_once 'includes/functions.inc.php';
-?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -23,16 +19,9 @@
           <li><a href="index.php">Home</a></li>
           <li><a href="discover.php">About Us</a></li>
           <li><a href="blog.php">Find Blogs</a></li>
-          <?php
-            if (isset($_SESSION["useruid"])) {
-              echo "<li><a href='profile.php'>Profile Page</a></li>";
-              echo "<li><a href='logout.php'>Logout</a></li>";
-            }
-            else {
-              echo "<li><a href='signup.php'>Sign up</a></li>";
-              echo "<li><a href='login.php'>Log in</a></li>";
-            }
-          ?>
+           <li><a href="signup.php">Sign up</a></li>
+            <li><a href="login.php">Log in</a></li>
+        
         </ul>
       </div>
     </nav>
